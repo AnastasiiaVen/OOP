722aee51f8e7c8235f742de3aa1bead317faa8a6
AnastasiiaVen <95999712+AnastasiiaVen@users.noreply.github.com>
2022-08-21 17:35:07 +0300

    Create AgeComparatorMinus.java

---
+++ Seminar_3_OOP/AgeComparatorMinus.java
@@ -0,0 +1,12 @@
+package Seminar3_OOP;
+
+import java.util.Comparator;
+
+public class AgeComparatorMinus implements Comparator<Person> {
+
+    @Override
+    public int compare(Person o1, Person o2) {
+        return o2.getAge() - o1.getAge();
+    }
+
+}
