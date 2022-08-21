9521e8808bfc8a2bf256e6f7e397e142dc165849
AnastasiiaVen <95999712+AnastasiiaVen@users.noreply.github.com>
2022-08-21 17:34:41 +0300

    Create AgeComporatorPlus.java

---
+++ Seminar_3_OOP/AgeComporatorPlus.java
@@ -0,0 +1,12 @@
+package Seminar3_OOPP;
+
+import java.util.Comparator;
+
+public class AgeComporatorPlus implements Comparator<Person>{
+
+    @Override
+    public int compare(Person o1, Person o2) {
+        return Integer.compare(o1.age, o2.age);
+    }
+    
+}
